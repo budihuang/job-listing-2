3288391a00efe0d72dd8c60f87ca89075bd73634
budihuang <huangpeizhong@huangpeizhongdeMacBook-Pro.local>
2017-01-21 22:53:02 +0800

    implement part 2

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -44,5 +44,11 @@ private
 def job_params
   params.require(:job).permit(:title, :description)
 
+  def require_is_admin
+      if !current_user.admin?
+        flash[:alert] = 'You are not admin'
+        redirect_to root_path
+      end
+    end
 
 end
